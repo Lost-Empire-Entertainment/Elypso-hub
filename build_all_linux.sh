2a4891a2764e20cf26be0242cc19254fdec03cb6
greenlaser <55558448+greeenlaser@users.noreply.github.com>
2025-04-09 15:33:55 +0300

    compilation improvements

---
+++ build_all_linux.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+PROJECT_ROOT="$(dirname "$(readlink -f "$0")")"
+cd "$PROJECT_ROOT"
+
+echo "====================================="
+echo "[INFO] Building hub in Release mode..."
+echo "====================================="
+echo ""
+
+bash "./build_linux_release.sh"
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Release build failed."
+    read -p "Press enter to continue..."
+    exit 1
+fi
+
+echo ""
+echo "====================================="
+echo "[INFO] Building hub in Debug mode..."
+echo "====================================="
+echo ""
+
+bash "./build_linux_debug.sh"
+if [ $? -ne 0 ]; then
+    echo "[ERROR] Debug build failed."
+    read -p "Press enter to continue..."
+    exit 1
+fi
+
+echo ""
+echo "====================================="
+echo "[SUCCESS] Finished building and installing hub!"
+echo "====================================="
+echo ""
+
+read -p "Press enter to continue..."
+exit 0
